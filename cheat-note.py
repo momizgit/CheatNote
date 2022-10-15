ebc5a0c921aa91b95738d7ae4bfb74d4a47d928f
Mohammad Moinul islam <71957374+momizgit@users.noreply.github.com>
2022-10-16 09:57:08 +1100

    Add files via upload

---
+++ cheat-note.py
@@ -0,0 +1,15 @@
+try:
+    import tkinter as tk  # using Python 3
+    from tkinter import ttk
+except ImportError:
+    import Tkinter as tk  # falls back to import from Python 2
+
+
+root = tk.Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+tk.mainloop()
