cfcd113ced4276844fb06051f6c9972bf9eeb27a
Mohammad Moin-ul islam <mohislam@deloitte.com.au,mohammadmoinul.islam@outlook.com>
2022-10-16 10:07:01 +1100

    Tested new column

--- cheat-note.py
+++ cheat-note.py
@@ -11,5 +11,5 @@ frm.grid()
 
 ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-
+ttk.Label(frm, text="c1r1").grid(column=100, row=1)
 tk.mainloop()
